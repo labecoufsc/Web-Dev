a6c346dc57a2114643c75e50f92fc1fb851a3cf3
yanocavalcante <yano.melo.cavalcante@gmail.com>
2024-02-21 12:42:55 -0300

    Adaptação para arquivos sass

--- landing/urls.py
+++ landing/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('boias/', views.boias, name='boias'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('index/', views.index, name='index'),
     path('mapaFlorianopolis/', views.mapaFlorianopolis, name='mapaFlorianopolis'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
