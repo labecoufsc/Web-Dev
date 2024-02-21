b749dfbf742beb6f5c46d57a1cdeb2cda244ef22
yanocavalcante <yano.melo.cavalcante@gmail.com>
2024-02-21 13:50:21 -0300

    Refatoração no esquema de views

--- landing/views.py
+++ landing/views.py
@@ -3,21 +3,21 @@ from django.template import loader
 
 
 def boias(request):
-    template = loader.get_template('boias.html')
+    template = loader.get_template('landing/boias.html')
     return HttpResponse(template.render())
 
 def about(request):
-    template = loader.get_template('about.html')
+    template = loader.get_template('landing/about.html')
     return HttpResponse(template.render())
 
 def contact(request):
-    template = loader.get_template('contact.html')
+    template = loader.get_template('landing/contact.html')
     return HttpResponse(template.render())
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('landing/index.html')
     return HttpResponse(template.render())
 
 def mapaFlorianopolis(request):
-    template = loader.get_template('mapaFlorianopolis.html')
+    template = loader.get_template('landing/mapaFlorianopolis.html')
     return HttpResponse(template.render())
